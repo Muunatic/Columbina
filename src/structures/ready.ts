e8b4969c98c0552c2f0955f2b135312431cc55b7
Muunatic <ichigoez0@gmail.com>
2022-06-26 04:50:15 +0700

    fix: fix reconnecting shard

--- src/structures/ready.ts
+++ src/structures/ready.ts
@@ -1,6 +1,6 @@
 import { client } from '../client';
 
-client.on('ready', async () => {
+client.on('shardReady', () => {
     console.log(client.user?.username + '#' + client.user?.discriminator + ': Hello, World!');
     client.user?.setActivity({ name: 'Hello, World! | /help', type: 'PLAYING' });
     client.user?.setStatus('online');
