8f1289e8bc7694009db2c372fa508eca7bf4de4c
Muunatic <ichigoez0@gmail.com>
2022-07-26 15:11:20 +0700

    refactor: refactor client path & structures

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
+import { client } from './src/client';
+import { token } from './src/data/config';
+
 require('./src/client');
+require('./structures/ready');
+require('./structures/handler');
+require('./structures/messageCreate');
+require('./structures/interactionCreate');
+require('./structures/error');
+require('./events/events');
+
+client.login(token);
