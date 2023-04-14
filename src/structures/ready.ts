29a6b8288f1961bda5db28ef087447ede2e0f009
Muunatic <ichigoez0@gmail.com>
2023-04-14 18:59:58 +0700

    style: formatting console and activity status

--- src/structures/ready.ts
+++ src/structures/ready.ts
@@ -2,13 +2,12 @@ console.info('Loading ready.ts');
 import { client, ActivityType } from '../client';
 
 client.once('ready', () => {
-    console.log(client.user.username + '#' + client.user.discriminator + ': Hello, World!');
+    console.log(client.user.username + '#' + client.user.discriminator + '\x1b[32m' + ': Hello, World!' + '\x1b[0m');
 });
 
 client.on('shardReady', () => {
     client.user.setActivity({
-        name: 'Hello, World!',
+        name: 'Hello, World! | /help',
         type: ActivityType.Playing
     });
-    client.user.setStatus('online');
 });
