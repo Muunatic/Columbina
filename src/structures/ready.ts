f03713e5db6dbda996bd2b9cb98d8d1c2aee9480
Muunatic <ichigoez0@gmail.com>
2023-06-27 11:36:05 +0700

    build: checkSemver for update

--- src/structures/ready.ts
+++ src/structures/ready.ts
@@ -1,8 +1,9 @@
 console.info('Loading ready.ts');
-import { client, ActivityType } from '../client';
+import { ActivityType, client } from '../client';
+import clientPackage from '../../package.json';
 
-client.once('ready', () => {
-    console.log(client.user.username + '#' + client.user.discriminator + '\x1b[32m' + ': Hello, World!' + '\x1b[0m');
+client.once('ready', async () => {
+    console.log(client.user.username + '#' + client.user.discriminator + ': \x1b[32m' + 'Hello, World!' + '\x1b[0m');
 });
 
 client.on('shardReady', () => {
@@ -11,3 +12,26 @@ client.on('shardReady', () => {
         type: ActivityType.Playing
     });
 });
+
+const checkSemver = async () => {
+    await fetch('https://api.github.com/repos/Muunatic/Columbina/releases/latest', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then((res) => {
+        return res.json();
+    }).then((data) => {
+        if (data) {
+            if (data.tag_name != clientPackage.version) {
+                return console.warn('\n\n \x1b[33m' + 'WARN' + '\x1B[0m' + ': ' + clientPackage.name.charAt(0).toUpperCase() + clientPackage.name.slice(1) + ' is ' + '\x1b[31moutdated\x1b[0m' + `! download new release \x1b[32mv${data.tag_name}\x1b[0m from \x1b[34m${data.html_url}\x1b[0m \n\n`);
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    });
+};
+checkSemver();
