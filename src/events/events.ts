19aa9d88dcefab45b5b1685abc6a2edeed35c192
Muunatic <ichigoez0@gmail.com>
2023-10-16 22:03:52 +0700

    refactor: add metadata Message interface

--- src/events/events.ts
+++ src/events/events.ts
@@ -1,5 +1,5 @@
 console.info('Loading events.ts');
-import { GuildQueue, Track, client, player } from '../client';
+import { GuildQueue, Message, Track, client, player } from '../client';
 
 client.on('shardDisconnect', () => {
     console.log('Disconnect');
@@ -9,22 +9,22 @@ client.on('shardReconnecting', () => {
     console.log('Reconnecting');
 });
 
-player.events.on('emptyChannel', (queue: GuildQueue<any>) => {
-    queue.metadata.channel.send('**Tidak ada member di voice**');
+player.events.on('emptyChannel', async (queue: GuildQueue<unknown>) => {
+    await (<Message>queue.metadata).channel.send('**Tidak ada member di voice**');
 });
 
-player.events.on('playerStart', (queue: GuildQueue<any>, track: Track) => {
-    queue.metadata.channel.send(`Memutar lagu **${track.title}**`);
+player.events.on('playerStart', async (queue: GuildQueue<unknown>, track: Track) => {
+    await (<Message>queue.metadata).channel.send(`Memutar lagu **${track.title}**`);
 });
 
-player.events.on('emptyQueue', (queue: GuildQueue<any>) => {
-    queue.metadata.channel.send('**Tidak ada music yang tersisa**');
+player.events.on('emptyQueue', async (queue: GuildQueue<unknown>) => {
+    await (<Message>queue.metadata).channel.send('**Tidak ada music yang tersisa**');
 });
 
-player.events.on('playerError', (error: any) => {
+player.events.on('playerError', (queue: GuildQueue<unknown>, error: Error) => {
     console.error(`Connection Error: ${error.message}`);
 });
 
-player.events.on('error', (error: any) => {
+player.events.on('error', (queue: GuildQueue<unknown>, error: Error) => {
     console.error(error.message);
 });
