de6bc32f60c17421aa7231eb4efefb311056dcdf
Muunatic <ichigoez0@gmail.com>
2023-10-16 22:00:53 +0700

    refactor: use VoiceConnection interface

--- src/structures/voice.ts
+++ src/structures/voice.ts
@@ -1,9 +1,9 @@
 console.info('Loading voice.ts');
 import { GuildQueue, player } from '../client';
-import { VoiceConnectionState, VoiceConnectionStatus } from '@discordjs/voice';
+import { VoiceConnection, VoiceConnectionState, VoiceConnectionStatus } from '@discordjs/voice';
 
-player.events.on('connection', (queue: GuildQueue) => {
-    queue.dispatcher.voiceConnection.on('stateChange', (oldState: VoiceConnectionState, newState: VoiceConnectionState) => {
+player.events.on('connection', (queue: GuildQueue<unknown>) => {
+    (queue.dispatcher.voiceConnection as unknown as VoiceConnection).on('stateChange', (oldState: VoiceConnectionState, newState: VoiceConnectionState) => {
         if (oldState.status === VoiceConnectionStatus.Ready && newState.status === VoiceConnectionStatus.Connecting) {
             queue.dispatcher.voiceConnection.configureNetworking();
         }
