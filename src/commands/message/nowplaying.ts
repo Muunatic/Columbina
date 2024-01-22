e7bb0c800ba3108b8557d606d944e8cb685e0e49
Muunatic <ichigoez0@gmail.com>
2024-01-22 17:49:04 +0700

    fix: avoid unavailable thumbnail image

--- src/commands/message/nowplaying.ts
+++ src/commands/message/nowplaying.ts
@@ -5,7 +5,11 @@ module.exports = {
     name: 'nowplaying',
     async execute(message: Message) {
         const queue = player.nodes.get(message.guild.id);
-        const srcInfo = ytdl.getInfo(queue.currentTrack.url);
+        const thumbnailInfo = await ytdl.getInfo(queue.currentTrack.url).then((data) => {
+            return data.videoDetails.thumbnails[0].url;
+        }).catch(() => {
+            return queue.currentTrack.thumbnail;
+        });
         if (queue?.isPlaying() == null || queue.isPlaying() == false) return message.reply('**Tidak ada music yang berjalan**');
         if (!message.member.voice.channel) return message.reply('**Kamu tidak divoice channel!**');
         if (message.guild.members.me.voice.channel && message.member.voice.channel.id !== message.guild.members.me.voice.channel.id) return message.reply('**Kamu tidak divoice channel yang sama!**');
@@ -13,7 +17,7 @@ module.exports = {
         const embed = new EmbedBuilder()
         .setColor('#89e0dc')
         .setTitle(queue.currentTrack.title)
-        .setThumbnail(queue.currentTrack.thumbnail || (await srcInfo).videoDetails.thumbnails[0].url)
+        .setThumbnail(thumbnailInfo)
         .setFooter({text: `Listening on ${queue.currentTrack.source}`, iconURL: message.client.user.avatarURL({extension: 'png', forceStatic: false, size: 1024})})
         .addFields(
             {name: 'Channel', value: `${queue.currentTrack.author}`, inline: true},
